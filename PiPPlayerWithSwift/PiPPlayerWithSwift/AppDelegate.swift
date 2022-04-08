a0944ba43aabe289bbe4310108bac1d5e3ca9d5f
kimhyeri <hyer1k@naver.com>
2022-04-08 16:43:53 +0900

    setup audiosession

--- PiPPlayerWithSwift/PiPPlayerWithSwift/AppDelegate.swift
+++ PiPPlayerWithSwift/PiPPlayerWithSwift/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+        
         return true
     }
 
