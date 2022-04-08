55f9399a40ba00814e791bf3af4b0324e245d7b1
kimhyeri <hyer1k@naver.com>
2022-04-08 16:39:07 +0900

    set up default methods

--- PiPPlayerWithSwift/PiPPlayerWithSwift/ViewController.swift
+++ PiPPlayerWithSwift/PiPPlayerWithSwift/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    var pipController: AVPictureInPictureController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        settingUpPiPController()
     }
-
-
+    
+    func settingUpPiPController() {
+        if AVPictureInPictureController.isPictureInPictureSupported() {
+            /*
+             put avplayer' playerLayer here
+             pipController = AVPictureInPictureController(playerLayer: self.playerLayer)
+             */
+            pipController?.delegate = self
+        }
+    }
 }
 
+extension ViewController: AVPictureInPictureControllerDelegate {
+    func pictureInPictureControllerWillStartPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
+        print("pictureInPictureControllerWillStartPictureInPicture")
+    }
+
+    func pictureInPictureControllerDidStartPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
+        print("pictureInPictureControllerDidStartPictureInPicture")
+    }
+
+    func pictureInPictureController(_ pictureInPictureController: AVPictureInPictureController, failedToStartPictureInPictureWithError error: Error) {
+        print("failedToStartPictureInPictureWithError")
+    }
+
+    func pictureInPictureControllerWillStopPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
+        print("pictureInPictureControllerWillStopPictureInPicture")
+    }
+
+    func pictureInPictureControllerDidStopPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
+        print("pictureInPictureControllerDidStopPictureInPicture")
+    }
+
+    func pictureInPictureController(_ pictureInPictureController: AVPictureInPictureController, restoreUserInterfaceForPictureInPictureStopWithCompletionHandler completionHandler: @escaping (Bool) -> Void) {
+        print("restoreUserInterfaceForPictureInPictureStopWithCompletionHandler")
+    }
+}
